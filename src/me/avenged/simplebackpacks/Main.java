906d4e65bd7ee898d709682582a74e0632896b17
Dragon0617 <dragon07172@gmail.com>
2016-09-30 21:42:39 -0400

    It's a simple backpack project.

--- src/me/avenged/simplebackpacks/Main.java
+++ src/me/avenged/simplebackpacks/Main.java
@@ -8,11 +8,14 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class Main extends JavaPlugin {
 
     public void onEnable(){
-
+    System.out.println("H");
     }
 
     public void onDisable(){
 
     }
+
+
+
 
 }
