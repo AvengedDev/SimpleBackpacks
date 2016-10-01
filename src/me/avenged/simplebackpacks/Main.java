aa79aef49b30be112117386d2abdb80979d8a000
BuildTools <unconfigured@null.spigotmc.org>
2016-09-30 21:35:59 -0400

    Initial commit

---
+++ src/me/avenged/simplebackpacks/Main.java
@@ -0,0 +1,18 @@
+package me.avenged.simplebackpacks;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by Avenged_ on 9/30/2016.
+ */
+public class Main extends JavaPlugin {
+
+    public void onEnable(){
+
+    }
+
+    public void onDisable(){
+
+    }
+
+}
